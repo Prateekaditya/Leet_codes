30e0978bfe75ea48d1370b8d65499cab32abc49b
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2024-08-26 01:06:43 +0530

    Time: 2 ms (100%), Space: 44.2 MB (96.57%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,25 +1,35 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> m = new HashMap<>();
-        
-        m.put('I', 1);
-        m.put('V', 5);
-        m.put('X', 10);
-        m.put('L', 50);
-        m.put('C', 100);
-        m.put('D', 500);
-        m.put('M', 1000);
-        
-        int ans = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
-                ans -= m.get(s.charAt(i));
-            } else {
-                ans += m.get(s.charAt(i));
-            }
+        int n= s.length();
+        if(s==null && s.length()==0){
+            return 0;
         }
-        
+          int ans= 0;
+          int prev = getValue(s.charAt(0));
+        for(int i=1;i<n; i++){
+            int curr= getValue(s.charAt(i));
+            if(prev<curr){
+                ans-=prev;
+            }
+            else{
+                ans+=prev;
+            }
+            prev= curr;
+        }
+        ans+=prev;
         return ans;
     }
+     private int getValue(char romanChar) {
+     switch (romanChar) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+           default:throw new IllegalArgumentException("Invalid Roman numeral: " + romanChar);
+     }
+     }   
 }
+       
