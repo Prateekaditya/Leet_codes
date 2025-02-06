ea4de1bd5dae0d5860a1107e86d135033b0e9dd2
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-02-07 02:36:17 +0530

    Time: 2 ms (100%), Space: 44.6 MB (91.8%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,35 +1,18 @@
 class Solution {
-    public int romanToInt(String s) {
-        int n= s.length();
-        if(s==null && s.length()==0){
-            return 0;
-        }
-          int ans= 0;
-          int prev = getValue(s.charAt(0));
-        for(int i=1;i<n; i++){
-            int curr= getValue(s.charAt(i));
-            if(prev<curr){
-                ans-=prev;
+  public int romanToInt(String s) {
+         int ans = 0, num = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            switch(s.charAt(i)) {
+                case 'I': num = 1; break;
+                case 'V': num = 5; break;
+                case 'X': num = 10; break;
+                case 'L': num = 50; break;
+                case 'C': num = 100; break;
+                case 'D': num = 500; break;
+                case 'M': num = 1000; break;
             }
-            else{
-                ans+=prev;
-            }
-            prev= curr;
+            if (4 * num < ans) ans -= num;
+            else ans += num;
         }
-        ans+=prev;
         return ans;
-    }
-     private int getValue(char romanChar) {
-     switch (romanChar) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-           default:throw new IllegalArgumentException("Invalid Roman numeral: " + romanChar);
-     }
-     }   
-}
-       
+    }}
