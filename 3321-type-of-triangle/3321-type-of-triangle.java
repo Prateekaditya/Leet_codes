bcb4856449567f6588840df78659760584c76759
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-05-19 21:56:29 +0530

    Time: 0 ms (100%), Space: 42.2 MB (43.93%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.java
+++ 3321-type-of-triangle/3321-type-of-triangle.java
@@ -1,20 +1,17 @@
 class Solution {
     public String triangleType(int[] nums) {
-        Arrays.sort(nums);
-        HashSet<Integer> set =new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            set.add(nums[i]);
-        }
-        if(nums[0]+nums[1]<=nums[2]){
-            return "none";
-        }
-        if(set.size()==1){
-            return "equilateral";
-        }
-        else if(set.size()==2){
-            return "isosceles";
-        }
-            return "scalene";
-        
+     int a =nums[0],b=nums[1],c=nums[2];
+     if(a+b<=c || b+c<=a || a+c<=b){
+        return "none";
+     }
+     else if(a==b && b==c){
+        return "equilateral";
+     }
+     else if(a==b||b==c||a==c){
+        return "isosceles";
+     }
+     else{
+        return "scalene";
+     }    
     }
 }
