000c83c54b5c1780efafe0ac22c482859941b72b
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-06-10 22:07:46 +0530

    Time: 5 ms (57.57%), Space: 48.3 MB (72.19%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,23 +1,22 @@
 class Solution {
-   public static List<Integer> lexicalOrder(int n) {
-        List<Integer> result = new ArrayList<>();
-        int current = 1;
-        
-        for (int i = 0; i < n; i++) {
-            result.add(current);
-            
-            if (current * 10 <= n) {
-                current *= 10;  // Go deeper (1 -> 10, 10 -> 100)
-            } else {
-                if (current >= n) {
-                    current /= 10;  // Move back up if current exceeds n
-                }
-                current++;
-                while (current % 10 == 0) {
-                    current /= 10;  // Remove trailing zeros
-                }
+    public List<Integer> lexicalOrder(int n) {
+    List<Integer> res = new ArrayList<>();
+    int current =1;
+    for(int i=0;i<n;i++){
+        res.add(current);
+        if(current *10 <=n){
+            current*=10;
+        }
+        else{
+            if(current >=n){
+                current /=10;
+            }
+            current++;
+            while(current %10==0){
+                current /=10;
             }
         }
-        return result;
+    }
+    return res;
     }
 }
