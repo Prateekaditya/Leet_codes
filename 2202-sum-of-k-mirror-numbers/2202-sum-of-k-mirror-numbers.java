6c4885a9285bf54bcc88f9db8db12db613797e9f
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-06-24 19:40:54 +0530

    Time: 937 ms (20%), Space: 44.8 MB (36.67%) - LeetHub

--- 2202-sum-of-k-mirror-numbers/2202-sum-of-k-mirror-numbers.java
+++ 2202-sum-of-k-mirror-numbers/2202-sum-of-k-mirror-numbers.java
@@ -1,46 +1,74 @@
 class Solution {
-
-    // This method returns the sum of the first n k-mirror numbers.
-    public long kMirror(int base, int count) {
-        long sum = 0; // Initialize sum of k-mirror numbers
-
-        // Loops indefinitely until 'count' number of k-mirror numbers are found
-        for (int length = 1; ; ++length) {
-            // Find the start and end range based on the half-length to construct palindrome prefixes
-            int start = (int) Math.pow(10, (length - 1) / 2);
-            int end = (int) Math.pow(10, (length + 1) / 2);
-
-            // Loop to generate all palindrome numbers of the current length
-            for (int i = start; i < end; i++) {
-                long palindrome = i;
-
-                // Construct the second half of the palindrome number based on the length's parity
-                for (int j = length % 2 == 0 ? i : i / 10; j > 0; j /= 10) {
-                    palindrome = palindrome * 10 + j % 10; // Append digits in reverse order to form the palindrome
-                }
-
-                // Convert palindrome to its representation in the given base
-                String baseRepresentation = Long.toString(palindrome, base);
-              
-                // Check if the base representation is a palindrome
-                if (isPalindrome(baseRepresentation.toCharArray())) {
-                    sum += palindrome; // Add current palindrome to sum
-                    if (--count == 0) { // Decrement count and check if 'n' k-mirror numbers are found
-                        return sum; // Return the sum if 'n' k-mirror numbers are found
-                    }
+    public long kMirror(int k, int n) {
+        long sum = 0;
+        int count = 0;
+        int length = 1;
+        
+        // Generate palindromes by length until we find n k-mirror numbers
+        while (count < n) {
+            // Generate all palindromes of current length
+            long start = (long) Math.pow(10, (length - 1) / 2);
+            long end = (long) Math.pow(10, (length + 1) / 2);
+            
+            for (long i = start; i < end && count < n; i++) {
+                long palindrome = generatePalindrome(i, length % 2 == 0);
+                
+                if (isPalindromeInBaseK(palindrome, k)) {
+                    sum += palindrome;
+                    count++;
                 }
             }
+            length++;
         }
+        
+        return sum;
     }
-
-    // This method checks if the character array forms a palindrome
-    private boolean isPalindrome(char[] charArray) {
-        // Compare characters from opposite ends moving towards the center
-        for (int i = 0, j = charArray.length - 1; i < j; i++, j--) {
-            if (charArray[i] != charArray[j]) {
-                return false; // Return false if mismatch is found
-            }
+    
+    // Generate palindrome from a given number
+    private long generatePalindrome(long root, boolean evenLength) {
+        String rootStr = String.valueOf(root);
+        StringBuilder sb = new StringBuilder(rootStr);
+        
+        if (evenLength) {
+            // For even length, mirror the entire root
+            sb.append(new StringBuilder(rootStr).reverse());
+        } else {
+            // For odd length, mirror all except the last digit
+            sb.append(new StringBuilder(rootStr.substring(0, rootStr.length() - 1)).reverse());
         }
-        return true; // Return true if the entire array is a palindrome
+        
+        return Long.parseLong(sb.toString());
+    }
+    
+    // Check if number is palindrome in base k
+    private boolean isPalindromeInBaseK(long num, int k) {
+        String baseKRepresentation = convertToBaseK(num, k);
+        return isPalindrome(baseKRepresentation);
+    }
+    
+    // Convert number to base k representation
+    private String convertToBaseK(long num, int k) {
+        if (num == 0) return "0";
+        
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            sb.append(num % k);
+            num /= k;
+        }
+        
+        return sb.reverse().toString();
+    }
+    
+    // Check if string is palindrome
+    private boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
