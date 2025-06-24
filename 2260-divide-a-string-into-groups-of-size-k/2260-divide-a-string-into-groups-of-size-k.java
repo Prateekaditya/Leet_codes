74d4bb6bbf2299e20a32dd593ce00930798053f4
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-06-25 00:11:54 +0530

    Time: 1 ms (91.31%), Space: 42.2 MB (95.89%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.java
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.java
@@ -1,35 +1,20 @@
 class Solution {
     public String[] divideString(String s, int k, char fill) {
-        // Calculate how many groups we need
-        int totalGroups = (s.length() + k - 1) / k;
-        
-        // Create result array to store all groups
-        String[] result = new String[totalGroups];
-        
-        // Process each group
-        for (int i = 0; i < totalGroups; i++) {
-            // Calculate start index for current group
-            int startIndex = i * k;
-            
-            // Calculate end index for current group
-            int endIndex = Math.min(startIndex + k, s.length());
-            
-            // Extract substring for current group
-            String group = s.substring(startIndex, endIndex);
-            
-            // If this is the last group and it's incomplete, add fill characters
-            if (group.length() < k) {
-                StringBuilder sb = new StringBuilder(group);
-                while (sb.length() < k) {
-                    sb.append(fill);
-                }
-                group = sb.toString();
+       int tg= (s.length()+k-1)/k;
+       String []res = new String[tg];
+       for(int i=0;i<tg;i++){
+        int si=i*k;
+        int end=Math.min(si+k,s.length());
+        String group = s.substring(si,end);
+        if(group.length()<k){
+            StringBuilder sb=new StringBuilder(group);
+            while(sb.length()<k){
+                sb.append(fill);
             }
-            
-            // Store the group in result array
-            result[i] = group;
+            group=sb.toString();
         }
-        
-        return result;
+        res[i]=group;
+       }
+       return res;
     }
 }
