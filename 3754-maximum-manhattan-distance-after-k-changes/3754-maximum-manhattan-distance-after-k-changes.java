9bd3c211ff1897a2c7a5491de80e5c3aa1eb11f7
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-06-25 21:17:30 +0530

    Time: 56 ms (55.39%), Space: 45.8 MB (28.67%) - LeetHub

--- 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.java
+++ 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.java
@@ -1,22 +1,30 @@
 class Solution {
     public int maxDistance(String s, int k) {
-        int ans = 0;
-        int north = 0, south = 0, east = 0, west = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == 'N') north++;
-            else if (c == 'S') south++;
-            else if (c == 'E') east++;
-            else if (c == 'W') west++;
-            
-            int x = Math.abs(north - south);
-            int y = Math.abs(east - west);
-            int MD = x + y;      //MD =Manhattan Distance
-            int dis = MD + Math.min(2 * k, i + 1 - MD); // Ensure changes are at most k
-            ans = Math.max(ans, dis);
+       int ans=0;
+       int north=0; 
+       int south=0; 
+       int east=0; 
+       int west=0;
+       for(int i=0;i<s.length();i++){
+        char c = s.charAt(i);
+        if(c=='N'){
+            north++;
         }
-        
-        return ans;
+        else if(c=='S'){
+            south++;
+        }
+        else if(c=='E'){
+            east++;
+        }
+        else if(c=='W'){
+            west++;
+        }
+        int x = Math.abs(north-south);
+        int y = Math.abs(east-west);
+        int MD= x+y;
+        int dis =MD + Math.min(2*k,i+1-MD);
+        ans=Math.max(dis,ans);
+       }
+       return ans; 
     }
 }
