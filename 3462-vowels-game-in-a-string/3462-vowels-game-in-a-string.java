e1fb7adc5390f3c552f307936fcda800d8533f96
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-09-13 02:09:43 +0530

    Time: 3 ms (95.92%), Space: 46.2 MB (5.61%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' ||ch=='o' || ch=='u'){
+                return true;
+            }
+        }
+        return false;
+    }
+}
