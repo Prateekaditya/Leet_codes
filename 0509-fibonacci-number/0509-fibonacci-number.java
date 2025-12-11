5ede40c38c6a9a14e68e79b45b6eabe35d0a6cd6
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-12-11 19:08:52 +0530

    Time: 31 ms (6.7%), Space: 46.1 MB (5.85%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        int []dp=new int[n+1];
+        if(n==0 || n==1){
+            return n;
+        }
+        if(dp[n]!=0){
+            return dp[n];
+        }
+        dp[n]=fib(n-1)+fib(n-2);
+        return dp[n];
+    }
+}
